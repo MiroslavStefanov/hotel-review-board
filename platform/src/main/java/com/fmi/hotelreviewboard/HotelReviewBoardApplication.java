1ede7325e3da0208aed2a3c2ce96066ba99e3157
mstef <mstefanov99@gmail.com>
2022-02-02 21:08:22 +0200

    Adding gititgnore

---
+++ platform/src/main/java/com/fmi/hotelreviewboard/HotelReviewBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.hotelreviewboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReviewBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelReviewBoardApplication.class, args);
+    }
+
+}
---
+++ platform/src/test/java/com/fmi/hotelreviewboard/HotelReviewBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.hotelreviewboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReviewBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
